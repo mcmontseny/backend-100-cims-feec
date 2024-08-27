75ba8ba5acdafd132ec755f3c279c7f5b982a60d
Marc Casamitjana i Montseny <mcmontseny.dev@gmail.com>
2024-08-27 19:33:21 +0200

    Add CORS

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api/v1/', routes);
 
 export default app;
